9bcd5e8df031c90b35eb96eaed1d14286f1a6b1f
Ziaul572 <z.haque572@gmail.com>
2020-03-13 21:17:35 +0600

    fixed

--- CodeForce/Dead Pixel.c
+++ CodeForce/Dead Pixel.c
@@ -1,12 +1,36 @@
 #include<stdio.h>
 int main()
 {
-    int n, i, j, a, b, x, y;
-    scanf("%d", &n);
-    for(i = 0; i < n; i++){
-        scanf("%d %d %d %d", &a, &b, &x, &y);
-
+    int tc, i, j, a, b, n, m;
+    scanf("%d", &tc);
+    int p, q, r, s, x;
+    int arr[100];
+    for(i = 0; i < tc; i++){
+        scanf("%d %d %d %d", &a, &b, &n, &m);
+        p = ((n + 1) - 1) * b;
+        q = (a - (n + 1)) * b;
+        r = ((m + 1) - 1) * a;
+        s = (b - (m + 1)) * a;
+        if(p >= q && p >= r && p >= s){
+            x = p;
+            //printf("%d", x);
         }
-
-    }
+        else if(q >= p && q >= r && q >= s){
+            x = q;
+            //printf("%d", x);
+        }
+        else if(r >= p && r >= q && r >= s){
+            x = r;
+            //printf("%d", x);
+        }
+        else {
+            x = s;
+            //printf("%d", x);
+        }
+        arr[i] = x;
+        }
+       // printf("%d", x);
+     for(i = 0; i < tc; i++){
+        printf("%d\n", arr[i]);
+     }
 }
