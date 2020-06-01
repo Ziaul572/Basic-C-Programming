82262f6c87431a3dfeb7805b7f5a4b080715c7ef
Ziaul572 <z.haque572@gmail.com>
2020-06-01 22:17:56 +0600

    pointers

--- Pointers/ReversedArray.c
+++ Pointers/ReversedArray.c
@@ -1,1 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int array[8] = { 0, 1, 2, 3, 4, 5, 6, 7 };
+    int i, *ptr, n = 7;
+    ptr = array;
+    int temp;
+
+    for(i = 0; i < 4; i++){
+        temp = *(ptr + i);
+        *(ptr + i) = *(ptr + n);
+        *(ptr + n)  = temp ;
+
+        //ptr++;  (not nessecery. caused many unnessesary increments.)
+        n--;
+      //  printf("%d",n);
+    }
+
+   // ptr = array;
+    for( i = 0; i < 8; i++){
+        printf("%d ", array[i]);
+       // ptr++;
+    }
+
+}
 
